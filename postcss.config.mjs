f86d6f402d97e55aa670a2388332d4d0c24c42bc
Anil Pal <anilcodes01@gmail.com>
2025-11-14 17:37:12 +0530

    fixed issues of ui

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,5 @@
 const config = {
-  theme: {
-    extend: {
-      animation: {
-        'fade-in': 'fadeIn 0.7s ease-in-out forwards',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-      },
-    },
-  },
+  
   plugins: ["@tailwindcss/postcss"],
 };
 
